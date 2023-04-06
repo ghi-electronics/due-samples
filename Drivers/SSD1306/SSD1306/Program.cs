a805eb153c30a906f29c906be1ec69be4179eef5
Dat Tran <dat.tran@ghielectronics.com>
2023-04-06 14:38:18 -0400

    add DrawString

--- Drivers/SSD1306/SSD1306/Program.cs
+++ Drivers/SSD1306/SSD1306/Program.cs
@@ -7,17 +7,8 @@ var dueController = new DUEController(port);
 
 var screen = new SSD1306.SSD1306(dueController);
 
-// Set left half of screen is 0, right half screen is 1
-for (var y = 0; y < screen.Height; y++) {
-    for (var x = 0; x < screen.Width; x++) {
-        if (x < screen.Width/2) {
-            screen.SetPixel(x, y, false);
-        }
-        else {
-            screen.SetPixel(x, y, true);
-        }
-    }
-}
+screen.Clear();
+screen.DrawString("DUE - SSD1306", 1, 5, 5);
 
 screen.Flush();
 
