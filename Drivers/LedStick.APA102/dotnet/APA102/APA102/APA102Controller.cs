22d72c85222a5bb9801ff9f963eed02c0f9ce9f1
Dat Tran <dat.tran@ghielectronics.com>
2023-04-12 13:53:56 -0400

    add APA102 driver

---
+++ Drivers/LedStick.APA102/dotnet/APA102/APA102/APA102Controller.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using GHIElectronics.DUE;
+
+namespace APA102 {
+    public class APA102Controller {
+        DUEController dueController;
+        public byte SlaveAddress { get; private set; }
+        // Qwiic LED Stick commands
+        const byte COMMAND_CHANGE_ADDRESS = 0xC7;
+        const byte COMMAND_CHANGE_LED_LENGTH = 0x70;
+        const byte COMMAND_WRITE_SINGLE_LED_COLOR = 0x71;
+        const byte COMMAND_WRITE_ALL_LED_COLOR = 0x72;
+        const byte COMMAND_WRITE_RED_ARRAY = 0x73;
+        const byte COMMAND_WRITE_GREEN_ARRAY = 0x74;
+        const byte COMMAND_WRITE_BLUE_ARRAY = 0x75;
+        const byte COMMAND_WRITE_SINGLE_LED_BRIGHTNESS = 0x76;
+        const byte COMMAND_WRITE_ALL_LED_BRIGHTNESS = 0x77;
+        const byte COMMAND_WRITE_ALL_LED_OFF = 0x78;
+
+        public int MaxLeds { get; private set; } = 10;
+        public APA102Controller(DUEController dueController, byte slaveAddress = 0x23) {
+            this.dueController = dueController;
+            this.SlaveAddress = slaveAddress;
+        }
+
+        public void Set(int ledIndex, byte red, byte green, byte blue ) {
+
+            if (ledIndex >= this.MaxLeds || ledIndex < 0)
+                throw new Exception("ledIndex is out of range.");
+
+            ledIndex++;
+            var data = new byte[5] { COMMAND_WRITE_SINGLE_LED_COLOR, (byte)ledIndex, red, green, blue };
+
+            this.dueController.I2c.Write(this.SlaveAddress, data);
+
+
+        }
+
+        public void Set(byte red, byte green, byte blue) {
+
+
+            var data = new byte[4] { COMMAND_WRITE_ALL_LED_COLOR, red, green, blue };
+
+            this.dueController.I2c.Write(this.SlaveAddress, data);
+
+
+        }
+
+        public void Brightness(uint ledIndex, uint brightness) {
+            if (brightness > 31)
+                brightness = 31;
+
+            var data = new byte[2] { COMMAND_WRITE_SINGLE_LED_BRIGHTNESS, (byte)brightness };
+
+            this.dueController.I2c.Write(this.SlaveAddress, data);
+        }
+
+        public void Clear() {
+            var data = new byte[1] { COMMAND_WRITE_SINGLE_LED_BRIGHTNESS };
+
+            this.dueController.I2c.Write(this.SlaveAddress, data);
+        }
+
+        public void ChangeAddress(byte newAddress) {
+            if (newAddress < 0x08 || newAddress > 0x77)
+                throw new Exception("new address must be in range [0x08, 0x77]");
+
+            if (newAddress == this.SlaveAddress)
+                return;
+
+            var data = new byte[2] { COMMAND_CHANGE_ADDRESS, newAddress };
+
+            this.dueController.I2c.Write(this.SlaveAddress, data);
+
+            this.SlaveAddress = newAddress;
+
+  
+        }
+
+        public void ChangeLedCount(int newCount) {
+            if (newCount > 255) {
+                throw new Exception("Support max 255 leds.");
+            }
+            var data = new byte[2] { COMMAND_CHANGE_LED_LENGTH, (byte)newCount };
+
+            this.dueController.I2c.Write(this.SlaveAddress, data);
+
+            this.MaxLeds = newCount;
+        }
+     
+
+
+    }
+}
---
+++ Drivers/LedStick.APA102/dotnet/APA102/APA102/Program.cs
@@ -0,0 +1,34 @@
+using System.Drawing;
+using APA102;
+using GHIElectronics.DUE;
+
+var port = DUEController.GetConnectionPort();
+
+var dueController = new DUEController(port);
+
+var ledstick = new APA102Controller(dueController);
+
+var colors = new int[] { 0xFFFFFF, 0xFF0000, 0x00FF00, 0x0000FF };
+
+var colorIndex = 0;
+
+while (true) {
+
+    var red = (byte)(colors[colorIndex % 4] >> 16);
+    var green = (byte)(colors[colorIndex % 4] >> 8);
+    var blue = (byte)(colors[colorIndex % 4] >> 0);
+
+    for (var i = 0; i < ledstick.MaxLeds; i++) {
+        if (i ==0) {
+            ledstick.Set(ledstick.MaxLeds-1, 0, 0, 0);
+        }
+        else
+            ledstick.Set(i - 1, 0, 0, 0);
+
+        ledstick.Set(i,red, green,blue);
+
+        Thread.Sleep(100);
+    }
+    colorIndex++;
+}
+
