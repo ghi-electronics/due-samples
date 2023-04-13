f9781e39af86f44a709529570efde3d6c6eea633
Dat Tran <dat.tran@ghielectronics.com>
2023-04-13 09:42:15 -0400

    add PIR_Sound_Button_Led

---
+++ PicoMate/dotnet/PIR_Sound_Button_Led/PIR_Sound_Button_Led/Program.cs
@@ -0,0 +1,61 @@
+// We need to connect pin 1 (Neo on Pico) to the RGB led
+// Button is connected to pin 26
+// PIR connected to pin 28
+// Buzzer connect to pin 27
+
+using GHIElectronics.DUE;
+
+var port = DUEController.GetConnectionPort();
+
+var dueController = new DUEController(port);
+
+
+var buttonPressed = false;
+var moveDetected = false;
+
+dueController.Neo.SetColor(0, 0, 255, 0); // Led Green
+dueController.Neo.Show(1);
+
+while (true) {
+    var buttonState = !dueController.Digital.Read(26, DUEController.Input.PULL_UP);
+    var pirState = dueController.Digital.Read(28, DUEController.Input.PULL_UP);
+
+    if (buttonPressed != buttonState) {
+        buttonPressed = buttonState;
+
+        if (buttonPressed) {
+            dueController.Neo.SetColor(0, 255, 255, 255); // led while
+        }
+        else {
+            dueController.Neo.SetColor(0, 0, 255, 0); // led green
+        }
+
+        dueController.Neo.Show(1);
+
+        dueController.System.Beep(27, 1000, 100);
+
+        Console.WriteLine("Button Pressed " + buttonPressed);
+
+    }
+
+    if (moveDetected != pirState) {
+        moveDetected = pirState;
+
+        if (moveDetected) {
+            dueController.Neo.SetColor(0, 255, 0, 0); // led read
+            Console.WriteLine("Moving detected!");
+        }
+        else {
+            dueController.Neo.SetColor(0, 0, 255, 0); // led green
+            Console.WriteLine("Moving stopped!");
+        }
+
+        dueController.Neo.Show(1);
+
+
+    }
+
+}
+
+
+
