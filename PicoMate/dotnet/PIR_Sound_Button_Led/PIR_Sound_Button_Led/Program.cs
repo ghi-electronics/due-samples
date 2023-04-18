91508997883c5760ce8c31bd25a328db5447e358
Dat Tran <dat.tran@ghielectronics.com>
2023-04-18 12:04:34 -0400

    add some notes

--- PicoMate/dotnet/PIR_Sound_Button_Led/PIR_Sound_Button_Led/Program.cs
+++ PicoMate/dotnet/PIR_Sound_Button_Led/PIR_Sound_Button_Led/Program.cs
@@ -2,6 +2,9 @@
 // Button is connected to pin 26
 // PIR connected to pin 28
 // Buzzer connect to pin 27
+// Default the led is green color
+// When user press the button, the color is while, a beep sound will be played.
+// When a motion detected, the led will be changed to red
 
 // 0.96 OlED Display: Refer to https://github.com/ghi-electronics/due-samples/tree/main/Drivers/Display/SSD1306
 // Temperature and Humidity: Refer to https://github.com/ghi-electronics/due-samples/tree/main/Drivers/Temperature-Humidity/SHT31
@@ -20,9 +23,13 @@ var moveDetected = false;
 dueController.Neo.SetColor(0, 0, 255, 0); // Led Green
 dueController.Neo.Show(1);
 
+var buttonPin = 26;
+var pirPin = 28;
+var beepPin = 27;
+
 while (true) {
-    var buttonState = !dueController.Digital.Read(26, DUEController.Input.PULL_UP);
-    var pirState = dueController.Digital.Read(28, DUEController.Input.PULL_UP);
+    var buttonState = !dueController.Digital.Read(buttonPin, DUEController.Input.PULL_UP);
+    var pirState = dueController.Digital.Read(pirPin, DUEController.Input.PULL_UP);
 
     if (buttonPressed != buttonState) {
         buttonPressed = buttonState;
@@ -36,7 +43,7 @@ while (true) {
 
         dueController.Neo.Show(1);
 
-        dueController.System.Beep(27, 1000, 100);
+        dueController.System.Beep(beepPin, 1000, 100);
 
         Console.WriteLine("Button Pressed " + buttonPressed);
 
