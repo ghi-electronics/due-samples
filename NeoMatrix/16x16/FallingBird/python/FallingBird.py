58f8f93b4d28a979b71939a6518a78f7510837f4
Dat Tran <dat.tran@ghielectronics.com>
2023-04-18 13:00:12 -0400

    add FallingBird.py

---
+++ NeoMatrix/16x16/FallingBird/python/FallingBird.py
@@ -0,0 +1,112 @@
+from DUE.DUEController import DUEController
+from DUE.Button import ButtonController
+import time
+import math
+import random
+
+port = DUEController.GetConnectionPort()
+dueController = DUEController(port)
+
+w=16      # Width of the matrix 
+u=4       # Player X position
+v=8       # Player Y position
+t=0       # Player tail Y offset
+b=15      # Wall X position
+h=4       # Wall height
+g=6       # Wall gap
+x = 0
+y = 0
+p = 0
+
+dueController.Button.Enable(ButtonController.Buttons.A, True)
+
+# Handle the player
+def plyr():
+    global w,u,v,t,b,h,g,x,y,p
+    if (dueController.Button.IsPressed(ButtonController.Buttons.A)):
+        if v > 0:
+            v -=1
+        t = 1
+    else:
+        if v < 15:
+            v = v + 0.5
+        t = -1
+    
+    x = u
+    y = math.trunc(v)
+    pxl()
+    dueController.Neo.SetColor(p, 64, 0, 64)
+    x=x-1
+    y=y+t
+    pxl()
+    dueController.Neo.SetColor(p, 64,0,64)
+    return
+
+
+# Update wall
+def wall():
+    global w,u,v,t,b,h,g,x,y,p
+    b=b-0.25
+    if (b <= 0):
+        b = 15
+        g = 4 + random.randrange(2)
+        h = 2 + random.randrange(6)
+
+    for i in range (2):
+        x = math.trunc(b) + i
+
+        for y in range (h+1):
+            pxl()
+            dueController.Neo.SetColor(p, 0,64,0)
+        
+        for y in range (h+g, 16):
+            pxl()
+            dueController.Neo.SetColor(p, 0,64,0)
+    return        
+    
+# Check collision
+def coll():
+    global w,u,v,t,b,h,g,x,y,p
+    i = math.trunc(b)       
+    if i != u and i != u -1:
+        return
+    
+    if v <= h:
+        die()
+
+    if v >= h+g:
+        die()
+
+# Player died
+def die():
+    global w,u,v,t,b,h,g,x,y,p
+    for i in range (0, 20):
+        x = (u - 2) + random.randrange(4)
+        y = (v - 2) + random.randrange(4)
+
+        pxl()
+        dueController.Neo.SetColor(p, random.randrange(64),0,0)
+        dueController.Neo.Show(256)
+    b = 15
+
+# Formula for index into 16x16 NeoPixel Matrix
+# p=pxl(x,y)
+
+def pxl():
+    global w,u,v,t,b,h,g,x,y,p
+    p = x*w+(x&1)*(w-1)+(1-2*(x&1))*y
+
+def Loop():
+    while True:
+        dueController.Neo.Clear()
+        plyr()
+        wall()
+        coll()
+        dueController.Neo.Show(256)
+        time.sleep(50/1000)
+
+Loop()
+
+
+
+
